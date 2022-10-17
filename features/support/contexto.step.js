89df80d442330e5f8207eb5877603fe2dccd53e7
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2022-10-17 00:58:37 -0300

    Criação Class Contexto

---
+++ features/support/contexto.step.js
@@ -0,0 +1,16 @@
+const axios = require("axios").default;
+const{setWorldConstructor} = require("@cucumber/cucumber");
+
+class Contexto {
+    async consultaApi() {
+        var {CEP} = this;
+        var url = `https://viacep.com.br/ws/${CEP}/json/`;
+        var response = await axios.get(url);
+        // console.log(response.data)
+        console.log(JSON.stringify(response.data))
+         return {
+            ...response.data
+         }
+   }
+}
+setWorldConstructor(Contexto)
